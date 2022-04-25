02065179fd7e16e808c876cda524e9bb765acca6
TommyLike <tommylikehu@gmail.com>
2022-04-25 20:20:13 +0800

    Move job definition to individual extend job plugin folder

---
+++ common/engine/kubernetes/extended_jobs/buildimagefromrelease/handler.go
@@ -0,0 +1,118 @@
+package buildimagefromrelease
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+	"go.uber.org/zap"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type JobImageBuildFromReleasePara struct {
+	Version      string   `json:"version"`
+	Packages     []string `json:"packages"`
+	Format       string   `json:"format"`
+	Architecture string   `json:"architecture"`
+}
+
+type BuildImagePackages struct {
+	Packages []string `json:"packages"`
+}
+
+type Handler struct {
+	logger     *zap.Logger
+	dataFolder string
+	namespace  string
+	name       string
+	paras      JobImageBuildFromReleasePara
+	templates  map[string][]byte
+}
+
+func NewHandler(dataFolder string, logger *zap.Logger) (*Handler, error) {
+	f, err := os.Stat(dataFolder)
+	if err != nil {
+		return nil, err
+	}
+	if !f.IsDir() {
+		return nil, errors.New(fmt.Sprintf("path %s is not a directory", dataFolder))
+	}
+	//check necessary yaml files
+	templates := make(map[string][]byte)
+	err = filepath.Walk(dataFolder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+		//read all yaml file
+		if strings.HasSuffix(path, ".yaml") {
+			templateFile, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer templateFile.Close()
+			bytes, err := ioutil.ReadAll(templateFile)
+			if err != nil {
+				return err
+			}
+			templates[info.Name()] = bytes
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Handler{
+		logger:     logger,
+		dataFolder: dataFolder,
+		paras:      JobImageBuildFromReleasePara{},
+		templates:  templates,
+	}, nil
+}
+
+func (h *Handler) GetJobArchitecture() string {
+	return strings.ToLower(h.paras.Architecture)
+}
+func (h *Handler) Initialize(namespace, name string, parameters map[string]interface{}) error {
+	h.name = name
+	h.namespace = namespace
+	err := mapstructure.Decode(parameters, &h.paras)
+	if err != nil {
+		return errors.New(fmt.Sprintf("unable to decode job specification %s for BuildImageFromRelease job", err))
+	}
+	// parse templates
+	wrapPackages := BuildImagePackages{
+		Packages: h.paras.Packages,
+	}
+	packages, err := json.MarshalIndent(wrapPackages, "", "\t")
+	if err != nil {
+		return err
+	}
+	variables := map[string]string{
+		"name":      h.name,
+		"namespace": h.namespace,
+		"version":   h.paras.Version,
+		"packages":  string(packages),
+		"format":    h.paras.Format,
+	}
+	for k, value := range h.templates {
+		resourceTemplate, err := template.New(k).Parse(string(value))
+		if err != nil {
+			return err
+		}
+		err = resourceTemplate.ExecuteTemplate(os.Stdout, k, variables)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (h *Handler) GetAllSerializedObjects() map[string][]byte {
+	return h.templates
+}
---
+++ common/engine/kubernetes/extended_jobs/plugins/interface.go
@@ -0,0 +1,17 @@
+package extended_jobs
+
+import (
+	"go.uber.org/zap"
+)
+
+type (
+	JobPlugin interface {
+		CreateJobHandler(dataFolder string, logger *zap.Logger) (JobHandler, error)
+	}
+
+	JobHandler interface {
+		GetJobArchitecture() string
+		Initialize(namespace, name string, parameters map[string]interface{}) error
+		GetAllSerializedObjects() map[string][]byte
+	}
+)
